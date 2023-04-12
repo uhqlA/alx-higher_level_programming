4060bbe1980c440c01b946c91f5f0982a48a86ff
uhqlA <alfie.alphonce@gmail.com>
2023-04-12 12:54:10 -0700

    task repeated

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a text file insertion function."""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file.
     Args:
